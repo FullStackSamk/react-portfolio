89de5af9ef9f719f649314d0a9376fdf1fc12ee0
Sampath <sampathkumar7534@gmail.com>
2025-06-05 00:22:56 -0400

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import NewsletterSignup from './components/NewsletterSignup';
-
-test('renders newsletter section', () => {
-  render(<NewsletterSignup />);
-  const heading = screen.getByText(/Join Our Newsletter/i);
-  expect(heading).toBeInTheDocument();
-});
